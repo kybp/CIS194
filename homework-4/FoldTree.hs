98148c682a404fe209ff150a0f2b364579a3aaba
Kyle Brown <picokyle@gmail.com>
2016-01-05 05:52:47 -0700

    fix foldTree

--- homework-4/FoldTree.hs
+++ homework-4/FoldTree.hs
@@ -6,15 +6,16 @@ data Tree a = Leaf
             deriving (Show, Eq)
 
 foldTree :: [a] -> Tree a
-foldTree = foldr foldTree' Leaf
+foldTree = foldr insert Leaf
   where
     -- Inserting into an empty tree results in a height of 0
-    foldTree' x Leaf = Node 0 Leaf x Leaf
+    insert x Leaf = Node 0 Leaf x Leaf
     -- Otherwise, our height is one more than that of our child
-    foldTree' x (Node h l y r)
+    insert x (Node _ l y r)
       -- If the left tree is taller, insert on the right
-      | (height l) > (height r) = Node h' l y $ foldTree' x r
-      | otherwise               = Node h (foldTree' x l) y r
-      where h' = h + 1
-            height (Node n _ _ _) = n
+      | (height l) > (height r) = Node (succ (height newRight)) l y newRight
+      | otherwise               = Node (succ (height newLeft)) newLeft y r
+      where height (Node n _ _ _) = n
             height Leaf = -1
+            newLeft  = insert x l
+            newRight = insert x r
