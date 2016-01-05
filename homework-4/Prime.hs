18971a5aca27f49e8d6bf4c171f295dcb400ebf1
Kyle Brown <picokyle@gmail.com>
2016-01-05 05:52:28 -0700

    change formatting

--- homework-4/Prime.hs
+++ homework-4/Prime.hs
@@ -7,4 +7,4 @@ cartProd xs ys = [(x, y) | x <- xs, y <- ys]
 sieveSundaram :: Integer -> [Integer]
 sieveSundaram n = (2 :) $ map ((+ 1) . (* 2)) $ filter valid [1..n]
   where is_and_js = cartProd [1..n `div` 2] [n `div` 2..n]
-        valid   x = not $ any (\(i, j) -> i + j + 2 * i * j <= x) is_and_js
+        valid x   = not $ any (\(i, j) -> i + j + 2 * i * j <= x) is_and_js
